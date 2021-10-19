4f6e7a08b92c76160fe2bda8bf28cf4d51292b62
Akvamarinika <rozespink@mail.ru>
2021-10-19 16:02:01 +0800

    Added task1.jar and read.me + refactoring

--- task1/src/main/java/ru/cft/focusstart/task1/Application.java
+++ task1/src/main/java/ru/cft/focusstart/task1/Application.java
@@ -1,21 +1,18 @@
 package ru.cft.focusstart.task1;
+
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            String table;
-            if (InputValidator.countArgs(args) && InputValidator.isInteger(args[0])){
-                table = createTable(args[0]);
-                System.out.println(table);
-            }
-        } catch (IllegalArgumentException ex) {
-            System.err.println(ex.getMessage());
-        }
+        String table;
 
+        if (InputValidator.countArgs(args) && InputValidator.isInteger(args[0])){
+            table = createTable(args[0]);
+            System.out.println(table);
+        }
 
     }
 
-    public static String createTable(String tableSize)throws IllegalArgumentException{
+    public static String createTable(String tableSize){
         int sizeTable = Integer.parseInt(tableSize);
 
         if (InputValidator.checkTableSize(sizeTable)) {
