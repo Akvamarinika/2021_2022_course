bc804ac8169d8bb2f94e5ccd226b186a8eacf6a2
Akvamarinika <rozespink@mail.ru>
2021-12-20 16:58:30 +0800

    update Message

--- task6/src/main/java/ru/cft/focusstart/task6/common/dto/Message.java
+++ task6/src/main/java/ru/cft/focusstart/task6/common/dto/Message.java
@@ -1,13 +1,49 @@
 package ru.cft.focusstart.task6.common.dto;
 
+import lombok.extern.slf4j.Slf4j;
+import ru.cft.focusstart.task6.common.MessageType;
+
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
 
-public class Message {
-    private String nickName;
-    private LocalDateTime dateTime;
+@Slf4j
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String text;
+    private final Set<String> connectionUsers;
 
-    public Message(String nickName, LocalDateTime dateTime) {
-        this.nickName = nickName;
-        this.dateTime = dateTime;
+    public Message(MessageType type, String text) {
+        this.text = text;
+        this.type = type;
+        this.connectionUsers = null;
     }
+
+    public Message(MessageType type, Set<String> connectionUsers) {
+        this.type = type;
+        this.text = null;
+        this.connectionUsers = connectionUsers;
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.text = null;
+        this.connectionUsers = null;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public Set<String> getConnectionUsers() {
+        return connectionUsers;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
+
 }
